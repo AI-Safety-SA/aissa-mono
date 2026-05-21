5e0ca5fd6c989054822ce9cade0f977a1c22ffdf
Charl <cyberCharl@users.noreply.github.com>
2026-05-21 11:59:52 +0200

    Update public website partner logos

--- apps/public-website/src/components/home/partner-logo-banner.tsx
+++ apps/public-website/src/components/home/partner-logo-banner.tsx
@@ -35,13 +35,6 @@ const partnerLogos: PartnerLogo[] = [
     width: 192,
   },
   {
-    alt: "UCT Partner Logo",
-    height: 996,
-    renderSize: "large",
-    src: "/images/partner-logos/uct-partner.png",
-    width: 996,
-  },
-  {
     alt: "Ashgro Logo",
     height: 311,
     renderSize: "small",
@@ -57,10 +50,10 @@ const partnerLogos: PartnerLogo[] = [
   },
   {
     alt: "Lambda Logo",
-    height: 128,
+    height: 100,
     renderSize: "medium",
-    src: "/images/partner-logos/lambda_logo_stacked_black.svg",
-    width: 100,
+    src: "/images/partner-logos/lambda_logo_horizontal_black.svg",
+    width: 463,
   },
   {
     alt: "Effective Altruism South Africa Logo",
